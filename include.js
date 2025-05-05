cb18ed0da9cf015221c2aba7b55621a63c169c3f
LuPeiheng <lupeihenged@gmail.com>
2025-05-05 18:25:55 +0800

    update page structure and fixed contact me

---
+++ include.js
@@ -0,0 +1,11 @@
+function loadComponent(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading component:', error));
+}
+
+loadComponent('header', 'static/header.html');
+loadComponent('footer', 'static/footer.html');
